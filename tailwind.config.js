c9819812d632b7e8b90325db6d60e37f928eb9d1
VuNguyen <nguyenminhvu.info@gmail.com>
2024-12-07 21:19:37 +0700

    First commit - Django project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.{html,js}',
+    './tasks/templates/tasks/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ tasks/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
+
+def toggle_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
